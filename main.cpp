c5887c55f2860fe98c2ba0ef66a993e604a0fadb
Florencia Rocca <florenciarocca@live.com>
2020-12-17 19:36:42 -0300

    asd

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ int main() {
 		printf("1-Get password\n");
 		printf("2-Add password\n");
 		printf("3-Close Hasher\n");
-
+		scanf("%s", &option);
+		/*C ES MEJOR Q PYTHOOOOON*/
 	}
 	printf("")
 	scanf("%s", &);
