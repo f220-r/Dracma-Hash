37b436909ca6ec76915b2b24abf3b4de0721f0f8
Florencia Rocca <florenciarocca@live.com>
2020-12-17 19:26:05 -0300

    trial_push

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+	string option;
+	bool close = false;
+	while (!close) {
+		printf("1-Get password\n");
+		printf("2-Add password\n");
+		printf("3-Close Hasher\n");
+
+	}
+	printf("")
+	scanf("%s", &);
+}
